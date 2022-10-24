4c9b7e3ea6462c7c38e60b8f168a382e489f5912
mslalith <mslalith.opensource@gmail.com>
2022-10-24 07:34:27 +0530

    trees: add TreeUtils to build tree from String

---
+++ trees/TreeUtils.kt
@@ -0,0 +1,28 @@
+package trees
+
+import java.util.PriorityQueue
+
+fun buildTreeNode(input: String): TreeNode {
+    val numStrings = input.split(" ".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+    val numbers = numStrings.map { it.toInt() }
+    if (numStrings.size != numbers.size) throw Exception ("Unable to parse input")
+
+    val queue = PriorityQueue<TreeNode>()
+    val root = TreeNode(numbers[0])
+    queue.add(root)
+
+    var count = 1
+    while (count < numbers.size && !queue.isEmpty()) {
+        val node = queue.remove()
+        numbers[count++].takeIf { it != -1 }?.let { left ->
+            node.left = TreeNode(left)
+            queue.add(node.left)
+        }
+        numbers[count++].takeIf { it != -1 }?.let { right ->
+            node.right = TreeNode(right)
+            queue.add(node.right)
+        }
+    }
+
+    return root
+}
