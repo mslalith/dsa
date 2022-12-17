f9b8d4baebd0a1d97644798d73173e0f554e4892
mslalith <mslalith.opensource@gmail.com>
2022-12-17 16:19:22 +0530

    linkedlist: solve PalindromeLinkedList

---
+++ src/linkedlist/problems/PalindromeLinkedList.kt
@@ -0,0 +1,65 @@
+package src.linkedlist.problems
+
+import src.core.Problem
+import src.core.TestCase
+import src.linkedlist.ListNode
+import src.utils.buildLinkedList
+
+class PalindromeLinkedList : Problem<ListNode?, Boolean>() {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) = PalindromeLinkedList().run()
+    }
+
+    override fun getTestCases(): Array<TestCase<ListNode?, Boolean>> = arrayOf(
+        TestCase(
+            input = buildLinkedList(input = "1,2,2,1", separator = ","),
+            output = true
+        ),
+        TestCase(
+            input = buildLinkedList(input = "1,21", separator = ","),
+            output = false
+        )
+    )
+
+    override fun solve(testCaseInput: ListNode?): Boolean {
+        return isPalindrome(testCaseInput)
+    }
+
+    private fun isPalindrome(head: ListNode?): Boolean {
+        val start: ListNode? = ListNode(0)
+        start?.next = head
+
+        var slow = start
+        var fast = start
+
+        while (fast?.next != null) {
+            slow = slow?.next
+            fast = fast.next?.next
+        }
+
+        slow?.next = reverseLinkedList(slow?.next)
+        slow = slow?.next
+
+        fast = head
+        while (slow != null && slow.`val` == fast?.`val`) {
+            slow = slow.next
+            fast = fast.next
+        }
+        if (slow == null) return true
+        return false
+    }
+
+    private fun reverseLinkedList(root: ListNode?): ListNode? {
+        var prev: ListNode? = null
+        var curr = root
+        while (curr != null) {
+            val next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        }
+        return prev
+    }
+}
