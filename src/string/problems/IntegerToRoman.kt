09c7da628a8cd248cf7d290c97c05873e8e0091d
mslalith <mslalith.opensource@gmail.com>
2023-01-07 16:05:01 +0530

    string: solve IntegerToRoman

---
+++ src/string/problems/IntegerToRoman.kt
@@ -0,0 +1,50 @@
+package src.string.problems
+
+import src.core.Problem
+import src.core.TestCase
+import java.lang.StringBuilder
+
+class IntegerToRoman : Problem<Int, String>() {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) = IntegerToRoman().run()
+    }
+
+    override fun getTestCases(): Array<TestCase<Int, String>> = arrayOf(
+        TestCase(input = 3, output = "III"),
+        TestCase(input = 58, output = "LVIII"),
+        TestCase(input = 1994, output = "MCMXCIV")
+    )
+
+    override fun solve(testCaseInput: Int): String {
+        return intToRoman(testCaseInput)
+    }
+
+    private fun intToRoman(num: Int): String {
+        val mapping = linkedMapOf(
+            1000 to "M",
+            900 to "CM",
+            500 to "D",
+            400 to "CD",
+            100 to "C",
+            90 to "XC",
+            50 to "L",
+            40 to "XL",
+            10 to "X",
+            9 to "IX",
+            5 to "V",
+            4 to "IV",
+            1 to "I"
+        )
+        val sb = StringBuilder()
+        var n = num
+        mapping.forEach { (value, roman) ->
+            while (n >= value) {
+                sb.append(roman)
+                n -= value
+            }
+        }
+        return sb.toString()
+    }
+}
