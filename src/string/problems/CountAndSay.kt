21b62fea29e92d948bd249d62e1318e93e642e80
mslalith <mslalith.opensource@gmail.com>
2023-01-08 12:01:09 +0530

    string: solve CountAndSay

---
+++ src/string/problems/CountAndSay.kt
@@ -0,0 +1,45 @@
+package src.string.problems
+
+import src.core.Problem
+import src.core.TestCase
+
+class CountAndSay : Problem<Int, String>() {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) = CountAndSay().run()
+    }
+
+    override fun getTestCases(): Array<TestCase<Int, String>> = arrayOf(
+        TestCase(input = 1, output = "1"),
+        TestCase(input = 4, output = "1211")
+    )
+
+    override fun solve(testCaseInput: Int): String {
+        return countAndSay(testCaseInput)
+    }
+
+    private fun countAndSay(n: Int): String {
+        if (n == 1) return "1"
+
+        var lastSay = "1"
+        val currentSay = StringBuilder()
+        repeat(times = n - 1) {
+            var lastChar = lastSay[0]
+            var count = 0
+            currentSay.clear()
+
+            lastSay.forEach { ch ->
+                if (ch == lastChar) count++ else {
+                    currentSay.append(count).append(lastChar)
+                    count = 1
+                    lastChar = ch
+                }
+            }
+            currentSay.append(count).append(lastChar)
+            lastSay = currentSay.toString()
+        }
+
+        return lastSay
+    }
+}
