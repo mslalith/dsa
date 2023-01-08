cad1acb8ba839176a5c89942d6325b9eff02390e
mslalith <mslalith.opensource@gmail.com>
2023-01-08 12:13:41 +0530

    math: solve FactorialTrailingZeroes

---
+++ src/math/problems/FactorialTrailingZeroes.kt
@@ -0,0 +1,35 @@
+package src.math.problems
+
+import src.core.Problem
+import src.core.TestCase
+
+class FactorialTrailingZeroes : Problem<Int, Int>() {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) = FactorialTrailingZeroes().run()
+    }
+
+    override fun getTestCases(): Array<TestCase<Int, Int>> = arrayOf(
+        TestCase(input = 3, output = 0),
+        TestCase(input = 5, output = 1),
+        TestCase(input = 20, output = 4),
+        TestCase(input = 100, output = 24)
+    )
+
+    override fun solve(testCaseInput: Int): Int {
+        return trailingZeroes(testCaseInput)
+    }
+
+    private fun trailingZeroes(n: Int): Int {
+        if (n < 0) return -1
+
+        var count = 0
+        var i = 5
+        while (i <= n) {
+            count += n / i
+            i *= 5
+        }
+        return count
+    }
+}
