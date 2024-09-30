3d95edfb565c801349f820e2d86eaba13e49e0ab
mslalith <mslalith.opensource@gmail.com>
2024-09-30 08:43:15 +0530

    array: solve FindDifferenceOfTwoArrays

---
+++ src/array/problems/FindDifferenceOfTwoArrays.kt
@@ -0,0 +1,42 @@
+package src.array.problems
+
+import src.core.Problem
+import src.core.TestCase
+
+class FindDifferenceOfTwoArrays : Problem<Pair<IntArray, IntArray>, List<List<Int>>>() {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) = FindDifferenceOfTwoArrays().run()
+    }
+
+    override fun getTestCases(): Array<TestCase<Pair<IntArray, IntArray>, List<List<Int>>>> = arrayOf(
+        TestCase(
+            input = intArrayOf(1,2,3) to intArrayOf(2,4,6),
+            output = listOf(listOf(1,3), listOf(4,6))
+        ),
+        TestCase(
+            input = intArrayOf(1,2,3,3) to intArrayOf(1,1,2,2),
+            output = listOf(listOf(3), listOf())
+        )
+    )
+
+    override fun solve(testCaseInput: Pair<IntArray, IntArray>): List<List<Int>> {
+        return findDifference(testCaseInput.first, testCaseInput.second)
+    }
+
+    private fun findDifference(nums1: IntArray, nums2: IntArray): List<List<Int>> {
+        val uniqueNums1 = hashSetOf<Int>()
+        nums1.forEach { uniqueNums1.add(it) }
+        nums2.forEach { uniqueNums1.remove(it) }
+
+        val uniqueNums2 = hashSetOf<Int>()
+        nums2.forEach { uniqueNums2.add(it) }
+        nums1.forEach { uniqueNums2.remove(it) }
+
+        return listOf(
+            uniqueNums1.toList(),
+            uniqueNums2.toList()
+        )
+    }
+}
