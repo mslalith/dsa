e12b1daab4be734dfd2d9d398fdc42a4c7dd1365
mslalith <mslalith.opensource@gmail.com>
2024-10-05 11:11:49 +0530

    graph: solve NumberOfProvinces

---
+++ src/graph/problems/NumberOfProvinces.kt
@@ -0,0 +1,80 @@
+package src.graph.problems
+
+import src.core.Problem
+import src.core.TestCase
+
+class NumberOfProvinces : Problem<Array<IntArray>, Int>() {
+    
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) = NumberOfProvinces().run()
+    }
+    
+    override fun getTestCases(): Array<TestCase<Array<IntArray>, Int>> = arrayOf(
+        TestCase(
+            input = arrayOf(
+                intArrayOf(1,1,0),
+                intArrayOf(1,1,0),
+                intArrayOf(0,0,1)
+            ),
+            output = 2
+        ),
+        TestCase(
+            input = arrayOf(
+                intArrayOf(1,0,0),
+                intArrayOf(0,1,0),
+                intArrayOf(0,0,1)
+            ),
+            output = 3
+        ),
+        TestCase(
+            input = arrayOf(
+                intArrayOf(1,0,0,0,0,0,0,0,0,1,0,0,0,0,0),
+                intArrayOf(0,1,0,1,0,0,0,0,0,0,0,0,0,1,0),
+                intArrayOf(0,0,1,0,0,0,0,0,0,0,0,0,0,0,0),
+                intArrayOf(0,1,0,1,0,0,0,1,0,0,0,1,0,0,0),
+                intArrayOf(0,0,0,0,1,0,0,0,0,0,0,0,1,0,0),
+                intArrayOf(0,0,0,0,0,1,0,0,0,0,0,0,0,0,0),
+                intArrayOf(0,0,0,0,0,0,1,0,0,0,0,0,0,0,0),
+                intArrayOf(0,0,0,1,0,0,0,1,1,0,0,0,0,0,0),
+                intArrayOf(0,0,0,0,0,0,0,1,1,0,0,0,0,0,0),
+                intArrayOf(1,0,0,0,0,0,0,0,0,1,0,0,0,0,0),
+                intArrayOf(0,0,0,0,0,0,0,0,0,0,1,0,0,0,0),
+                intArrayOf(0,0,0,1,0,0,0,0,0,0,0,1,0,0,0),
+                intArrayOf(0,0,0,0,1,0,0,0,0,0,0,0,1,0,0),
+                intArrayOf(0,1,0,0,0,0,0,0,0,0,0,0,0,1,0),
+                intArrayOf(0,0,0,0,0,0,0,0,0,0,0,0,0,0,1)
+            ),
+            output = 8
+        )
+    )
+    
+    override fun solve(testCaseInput: Array<IntArray>): Int {
+        return findCircleNum(testCaseInput)
+    }
+
+    private fun findCircleNum(isConnected: Array<IntArray>): Int {
+        val n = isConnected.size
+        val visited = BooleanArray(n) { false }
+        var count = 0
+
+        for (i in 0 until n) {
+            if (!visited[i]) {
+                count++
+                visited[i] = true
+                visitCity(isConnected, i, visited)
+            }
+        }
+
+        return count
+    }
+
+    private fun visitCity(connected: Array<IntArray>, i: Int, visited: BooleanArray) {
+        connected[i].forEachIndexed { index, value ->
+            if (value == 1 && !visited[index]) {
+                visited[index] = true
+                visitCity(connected, index, visited)
+            }
+        }
+    }
+}
