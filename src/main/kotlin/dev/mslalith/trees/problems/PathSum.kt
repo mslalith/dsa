e5056f3f01f9df254f8ebfde75d22f9e6af48938
mslalith <mslalith.opensource@gmail.com>
2024-10-22 14:41:32 +0530

    chore: housekeeping

--- src/main/kotlin/dev/mslalith/trees/problems/PathSum.kt
+++ src/main/kotlin/dev/mslalith/trees/problems/PathSum.kt
@@ -1,61 +1,45 @@
 package dev.mslalith.trees.problems
 
-import dev.mslalith.core.problem.TestCaseProblem
 import dev.mslalith.core.TestCase
+import dev.mslalith.core.problem.TestCaseProblem
 import dev.mslalith.trees.TreeNode
 import dev.mslalith.trees.buildTreeNode
 
-class PathSum : TestCaseProblem<PathSumParams, Boolean>() {
+class PathSum : TestCaseProblem<Pair<TreeNode?, Int>, Boolean>() {
 
     companion object {
         @JvmStatic
         fun main(args: Array<String>) = PathSum().runAll()
     }
 
-    override fun getTestCases(): Array<TestCase<PathSumParams, Boolean>> = arrayOf(
+    override fun getTestCases(): Array<TestCase<Pair<TreeNode?, Int>, Boolean>> = arrayOf(
         TestCase(
-            input = PathSumParams(treeString = "5,4,8,11,null,13,4,7,2,null,null,null,1", targetSum = 22),
+            input = buildTreeNode(input = "5,4,8,11,null,13,4,7,2,null,null,null,1") to 22,
             output = true
         ),
         TestCase(
-            input = PathSumParams(treeString = "-2,null,-3", targetSum = -5),
+            input = buildTreeNode(input = "-2,null,-3") to -5,
             output = true
         ),
         TestCase(
-            input = PathSumParams(treeString = "1,-2,-3,1,3,-2,null,-1", targetSum = -1),
+            input = buildTreeNode(input = "1,-2,-3,1,3,-2,null,-1") to -1,
             output = true
         ),
         TestCase(
-            input = PathSumParams(treeString = "1,2", targetSum = 1),
+            input = buildTreeNode(input = "1,2") to 1,
             output = false
         )
     )
 
-    override fun solve(testCaseInput: PathSumParams): Boolean {
-        return hasPathSum(
-            root = buildTreeNode(testCaseInput.treeString),
-            targetSum = testCaseInput.targetSum
-        )
+    override fun solve(testCaseInput: Pair<TreeNode?, Int>): Boolean {
+        return hasPathSum(testCaseInput.first, testCaseInput.second)
     }
 
     private fun hasPathSum(root: TreeNode?, targetSum: Int): Boolean {
-        if (root != null && root.`val` == targetSum && root.left == null && root.right == null) return true
         if (root == null) return false
+        if (root.`val` == targetSum && root.left == null && root.right == null) return true
 
         val nextTargetSum = targetSum - root.`val`
         return hasPathSum(root.left, nextTargetSum) || hasPathSum(root.right, nextTargetSum)
-    }
-}
-
-data class PathSumParams(
-    val treeString: String,
-    val targetSum: Int
-) {
-    override fun toString(): String {
-        return """
-            
-            root: $treeString
-            targetSum: $targetSum
-        """.trimIndent()
     }
 }
