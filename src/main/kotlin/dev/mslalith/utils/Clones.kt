2f8edb585dfbf2e987fab8f4311486275f81db30
mslalith <mslalith.opensource@gmail.com>
2024-10-23 18:36:30 +0530

    chore: add clone methods

--- src/main/kotlin/dev/mslalith/utils/Clones.kt
+++ src/main/kotlin/dev/mslalith/utils/Clones.kt
@@ -1,6 +1,28 @@
 package dev.mslalith.utils
 
-inline fun <reified T : Cloneable> Array<T>.createClone(): Array<T> = when (T::class) {
-    IntArray::class -> Array(size) { (get(it) as IntArray).clone() as T }
-    else -> throw UnsupportedOperationException("Not handled for ${T::class.simpleName} type")
+import dev.mslalith.trees.TreeNode
+
+@Suppress("UNCHECKED_CAST")
+fun <T : Any> T.createClone(): T = when (this) {
+    is IntArray -> (this as IntArray).clone() as T
+    is Array<*> -> (this as Array<*>).createClone() as T
+    is TreeNode -> this.createClone() as T
+    else -> throw UnsupportedOperationException("Type not handled")
+}
+
+private fun Array<*>.createClone(): Array<*> = when (first()) {
+    is IntArray -> Array(size) { (get(it) as IntArray).clone() }
+    else -> throw UnsupportedOperationException("Not handled for ${firstOrNull()?.javaClass?.simpleName} type")
+}
+
+private fun TreeNode.createClone(): TreeNode {
+    fun clone(oldNode: TreeNode?): TreeNode? {
+        if (oldNode == null) return null
+        val newNode = TreeNode(oldNode.`val`)
+        newNode.left = clone(oldNode.left)
+        newNode.right = clone(oldNode.right)
+        return newNode
+    }
+
+    return clone(this)!!
 }
