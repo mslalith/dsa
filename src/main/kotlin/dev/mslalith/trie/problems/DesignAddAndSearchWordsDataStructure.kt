30e38078705b34c48f7babe7e6b884698d3fb300
mslalith <mslalith.opensource@gmail.com>
2024-10-28 12:51:22 +0530

    trie: solve DesignAddAndSearchWordsDataStructure

--- src/main/kotlin/dev/mslalith/trie/problems/DesignAddAndSearchWordsDataStructure.kt
+++ src/main/kotlin/dev/mslalith/trie/problems/DesignAddAndSearchWordsDataStructure.kt
@@ -1,26 +1,76 @@
 package dev.mslalith.trie.problems
 
-import dev.mslalith.core.problem.SimpleProblem
+import dev.mslalith.core.TestCase
+import dev.mslalith.core.problem.TestCaseProblem
 import dev.mslalith.trie.TrieNode
+import dev.mslalith.trie.problems.DesignAddAndSearchWordsDataStructureType.Add
+import dev.mslalith.trie.problems.DesignAddAndSearchWordsDataStructureType.Search
 
-class DesignAddAndSearchWordsDataStructure : SimpleProblem<Unit>() {
+class DesignAddAndSearchWordsDataStructure : TestCaseProblem<List<DesignAddAndSearchWordsDataStructureType>, List<Boolean?>>() {
 
     companion object {
         @JvmStatic
         fun main(args: Array<String>) = DesignAddAndSearchWordsDataStructure().runForConsole()
     }
 
-    override fun runWithResult(): Result<Unit> = kotlin.runCatching {
-        with(WordDictionary()) {
-            addWord("bad")
-            addWord("dad")
-            addWord("mad")
-            check(!search("pad")) // return False
-            check(search("bad")) // return True
-            check(search(".ad")) // return True
-            check(search("b..")) // return True
+    override fun getTestCases(): Array<TestCase<List<DesignAddAndSearchWordsDataStructureType>, List<Boolean?>>> = arrayOf(
+        TestCase(
+            input = listOf(
+                Add("bad"),
+                Add("dad"),
+                Add("mad"),
+                Search("pad"),
+                Search("bad"),
+                Search(".ad"),
+                Search("b..")
+            ),
+            output = listOf(null, null, null, false, true, true, true)
+        ),
+        TestCase(
+            input = listOf(
+                Add("bad"),
+                Add("dad"),
+                Add("mad"),
+                Search("pad"),
+                Search("bad"),
+                Search(".ad"),
+                Search("b.."),
+                Search("ba")
+            ),
+            output = listOf(null, null, null, false, true, true, true, false)
+        ),
+        TestCase(
+            input = listOf(
+                Add("a"),
+                Add("a"),
+                Search("."),
+                Search("a"),
+                Search("aa"),
+                Search("a"),
+                Search(".a"),
+                Search("a.")
+            ),
+            output = listOf(null, null, true, true, false, true, false, false)
+        )
+    )
+
+    override fun solve(testCaseInput: List<DesignAddAndSearchWordsDataStructureType>): List<Boolean?> = buildList {
+        val dictionary = WordDictionary()
+        testCaseInput.forEach {
+            when (it) {
+                is Search -> add(dictionary.search(it.word))
+                is Add -> {
+                    dictionary.addWord(it.word)
+                    add(null)
+                }
+            }
         }
     }
+}
+
+sealed interface DesignAddAndSearchWordsDataStructureType {
+    data class Add(val word: String) : DesignAddAndSearchWordsDataStructureType
+    data class Search(val word: String) : DesignAddAndSearchWordsDataStructureType
 }
 
 private class WordDictionary {
@@ -31,23 +81,34 @@ private class WordDictionary {
         if (word.isEmpty()) return
 
         var currentNode = root
-        for (i in word.indices) {
-            val ch = word[i]
+
+        for (ch in word) {
             if (currentNode.children.contains(ch)) {
                 currentNode = currentNode.children.getValue(ch)
             } else {
-                val newNode = TrieNode(value = ch, isEnd = i == word.lastIndex)
+                val newNode = TrieNode(value = ch)
                 currentNode.children[ch] = newNode
                 currentNode = newNode
             }
         }
+
+        currentNode.isEnd = true
     }
 
     fun search(word: String): Boolean {
-        var currentNode = root
-        for (ch in word) {
-            currentNode = currentNode.children[ch] ?: return false
+        return searchFrom(root, word, 0)
+    }
+
+    private fun searchFrom(node: TrieNode<Char>, word: String, index: Int): Boolean {
+        var currentNode = node
+
+        for (i in index until word.length) {
+            when (val ch = word[i]) {
+                '.' -> return currentNode.children.values.any { searchFrom(it, word, i + 1) }
+                else -> currentNode = currentNode.children[ch] ?: return false
+            }
         }
+
         return currentNode.isEnd
     }
 }
