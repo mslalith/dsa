b0ec9feff5794d460a641aed00af74d3615c9abd
mslalith <mslalith.opensource@gmail.com>
2024-11-03 08:25:21 +0530

    graph: solve FindEventualSafeStates

---
+++ src/main/kotlin/dev/mslalith/graph/problems/FindEventualSafeStates.kt
@@ -0,0 +1,77 @@
+package dev.mslalith.graph.problems
+
+import dev.mslalith.core.TestCase
+import dev.mslalith.core.problem.TestCaseProblem
+
+class FindEventualSafeStates : TestCaseProblem<Array<IntArray>, List<Int>>() {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) = FindEventualSafeStates().runForConsole()
+    }
+
+    override fun getTestCases(): Array<TestCase<Array<IntArray>, List<Int>>> = arrayOf(
+        TestCase(
+            input = arrayOf(
+                intArrayOf(1, 2),
+                intArrayOf(2, 3),
+                intArrayOf(5),
+                intArrayOf(0),
+                intArrayOf(5),
+                intArrayOf(),
+                intArrayOf()
+            ),
+            output = listOf(2, 4, 5, 6)
+        ),
+        TestCase(
+            input = arrayOf(
+                intArrayOf(1, 2, 3, 4),
+                intArrayOf(1, 2),
+                intArrayOf(3, 4),
+                intArrayOf(0, 4),
+                intArrayOf()
+            ),
+            output = listOf(4)
+        )
+    )
+
+    override fun solve(testCaseInput: Array<IntArray>): List<Int> {
+        return eventualSafeNodes(testCaseInput)
+    }
+
+    private fun eventualSafeNodes(graph: Array<IntArray>): List<Int> {
+        val n = graph.size
+
+        val adjList = Array(n) { mutableListOf<Int>() }
+        for (i in graph.indices) {
+            graph[i].forEach { adjList[i].add(it) }
+        }
+
+        val visited = IntArray(n)
+        val safeNodes = BooleanArray(n)
+
+        for (i in graph.indices) {
+            if (visited[i] == 0) visitNode(i, visited, adjList, safeNodes)
+        }
+
+        val result = mutableListOf<Int>()
+        for (i in graph.indices) {
+            if (safeNodes[i]) result.add(i)
+        }
+        return result
+    }
+
+    private fun visitNode(node: Int, visited: IntArray, adjList: Array<MutableList<Int>>, safeNodes: BooleanArray): Boolean {
+        visited[node] = 2
+
+        for (neighbour in adjList[node]) {
+            if (visited[neighbour] == 0) {
+                if (!visitNode(neighbour, visited, adjList, safeNodes)) return false
+            } else if (visited[neighbour] == 2) return false
+        }
+
+        visited[node] = 1
+        safeNodes[node] = true
+        return true
+    }
+}
