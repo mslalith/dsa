bb503c531a441d4438b6a15a930407cb5fbab45a
mslalith <mslalith.opensource@gmail.com>
2024-11-12 09:58:27 +0530

    chore: housekeeping

--- src/main/kotlin/dev/mslalith/utils/Clones.kt
+++ src/main/kotlin/dev/mslalith/utils/Clones.kt
@@ -1,11 +1,13 @@
 package dev.mslalith.utils
 
+import dev.mslalith.linkedlist.ListNode
 import dev.mslalith.trees.TreeNode
 
 @Suppress("UNCHECKED_CAST")
 fun <T : Any> T.createClone(): T = when (this) {
     is IntArray -> (this as IntArray).clone() as T
     is Array<*> -> (this as Array<*>).createClone() as T
+    is ListNode -> this.createClone() as T
     is TreeNode -> this.createClone() as T
     else -> throw UnsupportedOperationException("Type not handled")
 }
@@ -14,6 +16,20 @@ private fun Array<*>.createClone(): Array<*> = if (isEmpty()) this else when (fi
     is IntArray -> Array(size) { (get(it) as IntArray).clone() }
     is CharArray -> Array(size) { (get(it) as CharArray).clone() }
     else -> throw UnsupportedOperationException("Not handled for ${firstOrNull()?.javaClass?.simpleName} type")
+}
+
+private fun ListNode.createClone(): ListNode {
+    val head = ListNode(`val`)
+    var newCurr: ListNode? = head
+    var curr = next
+
+    while (curr != null) {
+        newCurr?.next = ListNode(curr.`val`)
+        newCurr = newCurr?.next
+        curr = curr.next
+    }
+
+    return head
 }
 
 private fun TreeNode.createClone(): TreeNode {
