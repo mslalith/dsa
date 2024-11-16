7c8484babe36f92377fa6507e3f91286370e1de6
mslalith <mslalith.opensource@gmail.com>
2024-11-16 16:26:15 +0530

    dp: re-solve UniquePaths

--- src/main/kotlin/dev/mslalith/dp/problems/UniquePaths.kt
+++ src/main/kotlin/dev/mslalith/dp/problems/UniquePaths.kt
@@ -1,59 +1,81 @@
 package dev.mslalith.dp.problems
 
-import dev.mslalith.core.problem.TestCaseProblem
 import dev.mslalith.core.TestCase
+import dev.mslalith.core.problem.TestCaseProblem
 
-class UniquePaths : TestCaseProblem<UniquePathsParams, Int>() {
+class UniquePaths : TestCaseProblem<Pair<Int, Int>, Int>() {
 
     companion object {
         @JvmStatic
         fun main(args: Array<String>) = UniquePaths().runForConsole()
     }
 
-    override fun getTestCases(): Array<TestCase<UniquePathsParams, Int>> = arrayOf(
+    override fun getTestCases(): Array<TestCase<Pair<Int, Int>, Int>> = arrayOf(
         TestCase(
-            input = UniquePathsParams(m = 3, n = 2),
+            input = 3 to 2,
             output = 3
         ),
         TestCase(
-            input = UniquePathsParams(m = 3, n = 7),
+            input = 3 to 7,
             output = 28
         )
     )
 
-    override fun solve(testCaseInput: UniquePathsParams): Int {
-        return uniquePaths(testCaseInput.m, testCaseInput.n)
+    override fun solve(testCaseInput: Pair<Int, Int>): Int {
+        return uniquePaths(testCaseInput.first, testCaseInput.second)
     }
 
     private fun uniquePaths(m: Int, n: Int): Int {
-        val cache = hashMapOf<String, Int>()
-        return uniquePaths(0, 0, m - 1, n - 1, cache)
+        var prev = IntArray(n)
+        val curr = IntArray(n)
+
+        for (i in 0 until m) {
+            for (j in 0 until n) {
+                if (i == 0 && j == 0) curr[j] = 1
+                else {
+                    val top = if (i > 0) prev[j] else 0
+                    val left = if (j > 0) curr[j - 1] else 0
+                    curr[j] = top + left
+                }
+            }
+            prev = curr
+        }
+
+        return prev[n - 1]
     }
 
-    private fun uniquePaths(x: Int, y: Int, m: Int, n: Int, cache: HashMap<String, Int>): Int {
-        if (x == m && y == n) return 1
-        if (x > m || y > n) return 0
-        val rightKey = "${x + 1}-$y"
-        val downKey = "$x-${y + 1}"
-        val right = if (cache.containsKey(rightKey)) cache.getValue(rightKey) else {
-            uniquePaths(x + 1, y, m, n, cache).also { cache[rightKey] = it }
-        }
-        val down = if (cache.containsKey(downKey)) cache.getValue(downKey) else {
-            uniquePaths(x, y + 1, m, n, cache).also { cache[downKey] = it }
-        }
-        return right + down
-    }
-}
+    private fun uniquePathsDp(m: Int, n: Int): Int {
+        val dp = Array(m) { IntArray(n) }
 
-data class UniquePathsParams(
-    val m: Int,
-    val n: Int
-) {
-    override fun toString(): String {
-        return """
-            
-            m: $m
-            n: $n
-        """.trimIndent()
+        for (i in 0 until m) {
+            for (j in 0 until n) {
+                if (i == 0 && j == 0) dp[i][j] = 1
+                else {
+                    val top = if (i > 0) dp[i - 1][j] else 0
+                    val left = if (j > 0) dp[i][j - 1] else 0
+                    dp[i][j] = top + left
+                }
+            }
+        }
+
+        return dp[m - 1][n - 1]
+    }
+
+    private fun uniquePathsRecursive(m: Int, n: Int): Int {
+        val dp = Array(m) { IntArray(n) { -1 } }
+
+        fun findUniquePaths(i: Int, j: Int): Int {
+            if (i == 0 && j == 0) return 1
+            if (i < 0 || j < 0) return 0
+            if (dp[i][j] != -1) return dp[i][j]
+
+            val top = findUniquePaths(i, j - 1)
+            val left = findUniquePaths(i - 1, j)
+
+            dp[i][j] = top + left
+            return dp[i][j]
+        }
+
+        return findUniquePaths(m - 1, n - 1)
     }
 }
